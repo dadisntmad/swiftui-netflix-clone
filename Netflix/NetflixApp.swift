6b549d19f77909ed3b43f996ff1ba53633f778db
Ruslan Lantsov <ruslanlantsov1997@gmail.com>
2024-08-26 19:58:48 +0300

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Ruslan Lantsov on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
