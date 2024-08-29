f1dafa3ceeb03048371833140987d17e21838978
Ruslan Lantsov <ruslanlantsov1997@gmail.com>
2024-08-29 18:47:07 +0300

    implemented tab view

--- Netflix/Core/Views/Main/View/MainView.swift
+++ Netflix/Core/Views/Main/View/MainView.swift
@@ -2,7 +2,47 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            Text("Home")
+                .tabItem {
+                    Label(
+                        title: {
+                            Text("Home")
+                        },
+                        icon: {
+                            Image("home")
+                                .renderingMode(.template)
+                        }
+                    )
+                }
+            
+            Text("New & Hot")
+                .tabItem {
+                    Label(
+                        title: {
+                            Text("New & Hot")
+                        },
+                        icon: {
+                            Image("new")
+                                .renderingMode(.template)
+                        }
+                    )
+                }
+            
+            Text("More")
+                .tabItem {
+                    Label(
+                        title: {
+                            Text("More")
+                        },
+                        icon: {
+                            Image("more")
+                                .renderingMode(.template)
+                        }
+                    )
+                }
+        }
+        .tint(.white)
     }
 }
 
