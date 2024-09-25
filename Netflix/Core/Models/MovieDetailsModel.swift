eb32f9ee9caf29f22dfb58f78450b5ef4ab9cb5d
Ruslan Lantsov <ruslanlantsov1997@gmail.com>
2024-09-25 19:11:53 +0300

    created video model

--- Netflix/Core/Models/MovieDetailsModel.swift
+++ Netflix/Core/Models/MovieDetailsModel.swift
@@ -6,6 +6,7 @@ struct MovieDetailsModel: Identifiable, Codable {
     let posterPath: String?
     let releaseDate: String?
     let title: String?
+    let videos: Videos
     
     enum CodingKeys: String, CodingKey {
         case id
@@ -15,10 +16,15 @@ struct MovieDetailsModel: Identifiable, Codable {
         case posterPath = "poster_path"
         case releaseDate = "release_date"
         case title
+        case videos
     }
 }
 
 struct Genre: Identifiable, Codable {
     let id: Int
     let name: String
+}
+
+struct Videos: Codable {
+    let results: [VideoModel]
 }
---
+++ Netflix/Core/Models/VideoModel.swift
@@ -0,0 +1,6 @@
+struct VideoModel: Identifiable, Codable {
+    let id: String
+    let type: String
+    let site: String
+    let key: String
+}
